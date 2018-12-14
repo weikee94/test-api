249d3e799cfecf53c14e5f271d9b2148d9b8845f
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2018-12-14 17:28:36 +0800

    add post user login route

--- server/models/user.js
+++ server/models/user.js
@@ -87,6 +87,26 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 // what event u want to run before saved to database
 UserSchema.pre("save", function(next) {
   var user = this;
