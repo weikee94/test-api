09b94f98f9ac03af7a7a673892e8115f4f660cdc
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2018-12-14 16:20:44 +0800

    update jwt token for user sign up

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,69 @@
 var mongoose = require("mongoose");
+var validator = require("validator");
+var jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
-// mongoose model
-var User = mongoose.model("User", {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: value => {
+        return validator.isEmail(value);
+      },
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: { type: String, required: true },
+      token: { type: String, required: true }
+    }
+  ]
 });
+
+// we only want give back id and email (those sensitive value such as password should be hidden)
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ["_id", "email"]);
+};
+
+// create instance methods (self create method)
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+
+  // generate access and token
+  var access = "auth";
+  var token = jwt
+    .sign({ _id: user._id.toHexString(), access }, "asdqwerdg")
+    .toString();
+
+  // push to user array
+  user.tokens = user.tokens.concat([
+    {
+      access,
+      token
+    }
+  ]);
+
+  return user.save().then(() => {
+    console.log(user);
+    return token;
+  });
+};
+
+// mongoose model
+var User = mongoose.model("User", UserSchema);
 
 module.exports = {
   User: User
--- server/server.js
+++ server/server.js
@@ -120,6 +120,25 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 
+// setup the users
+app.post("/users", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  var user = new User(body);
+
+  // save to database
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
